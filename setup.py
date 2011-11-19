cd80e7170059984c4b6f764a4b00b089f799c7c0
titusz <tp@py7.de>
2011-11-20 00:03:05 +0100

    bumped version to 0.7.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.7.2'
+version = '0.7.3'
 
 setup(name='py7file',
     version=version,
